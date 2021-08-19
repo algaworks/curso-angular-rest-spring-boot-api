ce139f754801b8c3abc610184f9621dbb7942455
Alex <alex.silva@algaworks.com.br>
2021-08-19 15:39:47 -0300

    Adicionado aula 4.1 com código-fonte atualizado para Spring Boot 2.5.0

---
+++ 4.1 Removendo pessoa com DELETE - Spring Boot 2.5.0/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -0,0 +1,57 @@
+package com.example.algamoney.api.resource;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.PessoaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);		
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+		return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		this.pessoaRepository.deleteById(codigo);
+	}
+
+	
+}
