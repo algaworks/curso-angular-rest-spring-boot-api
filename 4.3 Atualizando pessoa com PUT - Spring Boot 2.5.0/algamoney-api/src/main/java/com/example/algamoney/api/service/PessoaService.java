ca7a30b3be91fe21a7a28ddfe90d5662f1dbfe5c
Alex <alex.silva@algaworks.com.br>
2021-08-19 15:43:46 -0300

    Adicionado aula 4.3 com código-fonte atualizado para Spring Boot 2.5.0

---
+++ 4.3 Atualizando pessoa com PUT - Spring Boot 2.5.0/algamoney-api/src/main/java/com/example/algamoney/api/service/PessoaService.java
@@ -0,0 +1,24 @@
+package com.example.algamoney.api.service;
+
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.PessoaRepository;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaService {
+ 	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaRepository.findById(codigo)
+                                .orElseThrow(() -> new EmptyResultDataAccessException(1));
+		
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}   
+}
