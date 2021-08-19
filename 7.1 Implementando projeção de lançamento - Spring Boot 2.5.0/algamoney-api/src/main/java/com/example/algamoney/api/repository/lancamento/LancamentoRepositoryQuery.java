43ff9971581512decc48bdc365d8b1f717fd8ed0
Alex <alex.silva@algaworks.com.br>
2021-08-19 17:14:41 -0300

    Adicionado aula 7.1 com código-fonte atualizado para Spring Boot 2.5.0

---
+++ 7.1 Implementando projeção de lançamento - Spring Boot 2.5.0/algamoney-api/src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -0,0 +1,16 @@
+package com.example.algamoney.api.repository.lancamento;
+
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import com.example.algamoney.api.repository.projection.ResumoLancamento;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface LancamentoRepositoryQuery {
+
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
+
+}
---
+++ 7.1 Implementando projeção de lançamento - Spring Boot 2.5.0/algamoney-api/src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -0,0 +1,32 @@
+package com.example.algamoney.api.service;
+
+import java.util.Optional;
+
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LancamentoService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Autowired 
+	private LancamentoRepository lancamentoRepository;
+
+	public Lancamento salvar(Lancamento lancamento) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		if (pessoa.isEmpty() || pessoa.get().isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+		
+		return lancamentoRepository.save(lancamento);
+	}
+	
+}
