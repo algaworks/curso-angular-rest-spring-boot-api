2c10bc370bc58e3e8814b510f2a12999f20db00c
Alisson Gomes <44034024+AlsGomes@users.noreply.github.com>
2022-07-14 20:47:01 -0300

    Adicionado aula 3.11 com código-fonte atualizado para Spring Boot 2.7.0

---
+++ 3.11 Desafio - Criando o cadastro de pessoa - Spring Boot 2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -0,0 +1,46 @@
+package com.example.algamoney.api.resource;
+
+import java.net.URI;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.PessoaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+			response.setHeader("Location", uri.toASCIIString());
+			
+			return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+		return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
+	}
+	
+}
