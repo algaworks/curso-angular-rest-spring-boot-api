e1ba7d1c1a56ba11ef2352d40505d3628f99b447
AlsGomes <als_08.net@hotmail.com>
2022-07-21 20:43:40 -0300

    Adicionado aula 5.9 com código-fonte atualizado para Spring Boot 2.7.0

---
+++ 5.9 Implementando a paginação de lançamentos - Spring Boot 2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/event/RecursoCriadoEvent.java
@@ -0,0 +1,26 @@
+package com.example.algamoney.api.event;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationEvent;
+
+public class RecursoCriadoEvent extends ApplicationEvent {
+    private static final long serialVersionUID = 1L;
+	
+	private HttpServletResponse response;
+	private Long codigo;
+
+	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long codigo) {
+		super(source);
+		this.response = response;
+		this.codigo = codigo;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+}
