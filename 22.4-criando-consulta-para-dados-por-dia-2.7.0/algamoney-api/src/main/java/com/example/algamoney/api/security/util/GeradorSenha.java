ace9e5e524f500c7f783ad4af122db8fd8f32653
AlsGomes <als_08.net@hotmail.com>
2022-09-09 22:37:21 -0300

    Adicionado aula 22.4 com código-fonte atualizado para Spring Boot 2.7.0

---
+++ 22.4-criando-consulta-para-dados-por-dia-2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/security/util/GeradorSenha.java
@@ -0,0 +1,10 @@
+package com.example.algamoney.api.security.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GeradorSenha {
+    public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("@ngul@r0"));
+	}
+}
