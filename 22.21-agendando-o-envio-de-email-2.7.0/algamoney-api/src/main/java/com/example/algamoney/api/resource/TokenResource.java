b9559832655d62a3923956cf977affc6f37b5b29
AlsGomes <als_08.net@hotmail.com>
2022-09-14 20:14:20 -0300

    Adicionado aula 22.21 com código-fonte atualizado para Spring Boot 2.7.0

---
+++ 22.21-agendando-o-envio-de-email-2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/resource/TokenResource.java
@@ -0,0 +1,34 @@
+package com.example.algamoney.api.resource;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+	
+	@Autowired
+	private AlgamoneyApiProperty algamoneyApiProperty;
+
+	@DeleteMapping("/revoke")
+	public void revoke(HttpServletRequest req, HttpServletResponse resp) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(algamoneyApiProperty.getSeguranca().isEnableHttps());
+		cookie.setPath(req.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+		
+		resp.addCookie(cookie);
+		resp.setStatus(HttpStatus.NO_CONTENT.value());
+	}
+	
+}
