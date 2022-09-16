3ebb762977e145e1b2fa43a8022e7a88e1909a9f
AlsGomes <als_08.net@hotmail.com>
2022-09-15 22:16:04 -0300

    Adicionado aula 22.23 com código-fonte atualizado para Spring Boot 2.7.0

---
+++ 22.23-criando-a-entidade-contato-para-suportar-mestre-detalhe-2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.example.algamoney.api.security;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import com.example.algamoney.api.model.Usuario;
+import com.example.algamoney.api.repository.UsuarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
+		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
+	}
+
+	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		usuario.getPermissoes().forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
+		return authorities;
+	}
+
+}
