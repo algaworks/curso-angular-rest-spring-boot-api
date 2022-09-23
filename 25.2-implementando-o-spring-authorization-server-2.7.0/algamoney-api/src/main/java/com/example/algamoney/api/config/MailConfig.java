aa50631d12e24c25a82458d09fac6eb2eb2aa074
AlsGomes <als_08.net@hotmail.com>
2022-09-23 16:27:52 -0300

    Adicionado aula 25.2 com código-fonte atualizado para Spring Boot 2.7.0

---
+++ 25.2-implementando-o-spring-authorization-server-2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/config/MailConfig.java
@@ -0,0 +1,35 @@
+package com.example.algamoney.api.config;
+
+import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+	@Autowired
+	private AlgamoneyApiProperty property;
+
+	@Bean
+	public JavaMailSender javaMailSender() {
+		Properties props = new Properties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", true);
+		props.put("mail.smtp.starttls.enable", true);
+		props.put("mail.smtp.connectiontimeout", 10000);
+
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setJavaMailProperties(props);
+		mailSender.setHost(property.getMail().getHost());
+		mailSender.setPort(property.getMail().getPort());
+		mailSender.setUsername(property.getMail().getUsername());
+		mailSender.setPassword(property.getMail().getPassword());
+
+		return mailSender;
+	}
+}
---
+++ 25.2-implementando-o-spring-authorization-server-2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.example.algamoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.algamoney.api.model.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
+}
